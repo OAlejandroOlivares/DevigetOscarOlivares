95a80167404a297c5e3f4ef477828194d022f5a9
Oscar Olivares <oscarsel36@hotmail.com>
2021-04-17 21:03:44 -0500

    Get entries function in viewmodel

--- app/src/main/java/com/app/devigetoscarolivares/MainActivity.kt
+++ app/src/main/java/com/app/devigetoscarolivares/MainActivity.kt
@@ -2,23 +2,37 @@ package com.app.devigetoscarolivares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.*
 import com.app.devigetoscarolivares.ViewModel.RedditVM
+import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    private lateinit var viewmodel: RedditVM
+    private lateinit var authJob : CompletableJob
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setupViewModel()
     }
 
     private fun setupViewModel() {
-        val viewmodel = ViewModelProvider(this).get(RedditVM::class.java)
-        CoroutineScope(IO).launch {
-            val a = viewmodel.authReddit()
+        authJob = Job()
+        authJob.invokeOnCompletion {
+            if (it == null) {
+                viewmodel.getTopEntries(null)
+            }
+        }
+        viewmodel = ViewModelProvider(this).get(RedditVM::class.java)
+        CoroutineScope(IO + authJob).launch {
+            val a = viewmodel.authReddit(authJob)
             //val b = viewmodel.testApi()
         }
     }
--- app/src/main/java/com/app/devigetoscarolivares/ViewModel/RedditVM.kt
+++ app/src/main/java/com/app/devigetoscarolivares/ViewModel/RedditVM.kt
@@ -1,27 +1,48 @@
 package com.app.devigetoscarolivares.ViewModel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.app.devigetoscarolivares.Models.RedditEntry
+import kotlinx.coroutines.CompletableJob
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
 import java.util.*
+import kotlin.collections.ArrayList
 
 class RedditVM : ViewModel() {
 
     private val client = OkHttpClient()
-    var entries : MutableLiveData<List<RedditEntry>> = MutableLiveData()
+    var entries : List<RedditEntry> = ArrayList<RedditEntry>()
+    var token :String? = null
     val formBody = FormBody.Builder()
         .add("grant_type", "https://oauth.reddit.com/grants/installed_client")
         .add("device_id", UUID.randomUUID().toString())
         .build()
     private val CLIENT_ID ="WoWu_4334wTkwQ"
-    fun getTopEntries(after: String?) : LiveData<List<RedditEntry>> {
+
+
+    fun getTopEntries(after: String?) : List<RedditEntry> {
+        val request = Request.Builder()
+            //.url("https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=token&state=RANDOM_STRING&redirect_uri=http://127.0.0.1&scope=read")
+            .url("https://oauth.reddit.com/top")
+            .addHeader("Authorization","bearer ${token}")
+            .addHeader("User-agent","devigetOscar 1.0 by /u/No_Butterscotch2578")
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) { //throw IOException("Unexpected code $response")
+                Log.d("asd",response.message)
+            }
+            var json = JSONObject(response.body!!.string())
+            token = json.getString("access_token")
+        }
         return entries
     }
 
-    fun authReddit() : Boolean{
+    fun authReddit(authJob: CompletableJob) {
         val request = Request.Builder()
             //.url("https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=token&state=RANDOM_STRING&redirect_uri=http://127.0.0.1&scope=read")
             .url("https://www.reddit.com/api/v1/access_token")
@@ -30,15 +51,13 @@ class RedditVM : ViewModel() {
             .build()
 
         client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-            for ((name, value) in response.headers) {
-                println("$name: $value")
+            if (!response.isSuccessful) { //throw IOException("Unexpected code $response")
+                authJob.completeExceptionally(IOException("Unexpected code $response"))
             }
-
-            println(response.body!!.string())
+            var json = JSONObject(response.body!!.string())
+            token = json.getString("access_token")
+            authJob.complete()
         }
-        return false
     }
 
     fun testApi(){
